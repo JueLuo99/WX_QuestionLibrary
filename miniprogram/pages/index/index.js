17299868692b863180c7f3dd62b52df27bc1aadc
GuokeNo1 <1962908113@qq.com>
2020-07-01 09:15:15 +0800

    初次提交

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,34 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    avatarUrl: '../../images/user-unlogin.png',
+    usingTools: [[{ "name": "联系", "active": "clickTeacherTable", "type": 0 }, { "name": "模拟", "active": "clicktable", "type": 0 }], [{ "name": "客服", "type": 1 }]]
+  },
+  onShow: function(){
+    this.onPullDownRefresh()
+  },
+  onLoad: function() {
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+              this.setData({ isGetInfo: true })
+            }
+          })
+        }
+      }
+    })
+
+  },  
+  onPullDownRefresh: function(e){
+  }
+})
