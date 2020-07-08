d6c11161699629521238d9893b74a6b253ddc515
JueLuo <jueluo99@gmail.com>
2020-07-08 16:13:52 +0800

    调整页面代码以匹配数据库

--- miniprogram/pages/exercise/exercise.js
+++ miniprogram/pages/exercise/exercise.js
@@ -7,7 +7,7 @@ Page({
   data: {
 
     // 这个 List 里面存抽到的题目的 ID
-    questionList: [{"question":"题目1题目aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa","choices":[{"answer":"这是选项A","isTrue":false,"class":"choice","selected":false},{"answer":"选项BBBBBBBBBBBBB阿巴BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB1111111BBBBBBBBBBBBBBBBB","isTrue":true,"class":"choice","selected":false}],"answerNumber":1}],
+    questionList: [{"question":"这是亿个题目","choices":[{"answers":"这是选项A","isTrue":false,"class":"choice","selected":false},{"answers":"选项BBBBBBBBBBBBB阿巴BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB1111111BBBBBBBBBBBBBBBBB","isTrue":true,"class":"choice","selected":false}],"answerNumber":1}],
     end: false,
     // 这个存放问题题干
     question: "1",
@@ -18,24 +18,18 @@ Page({
   },
   getQuestion: function(){
     const db = wx.cloud.database({
-      env: "tiku"
+      env: "tiku-na1fl"
     });
     const $ = db.command;
-    // db.collection('WangAn').aggregate().sample({size:1}).end().then(res=>{console.log(res)});
-//     db.collection('WangAn')
-//       .aggregate()
-//       .sample({
-//         size:1
-//       })
-//       .end().then( res => { console.log(res);});
-  db.collection('WangAn').aggregate().sample({size: 1}).end().then(res => {
+
+  db.collection('ceshi').aggregate().sample({size: 1}).end().then(res => {
     console.log(res.list);
     this.data.questionList[0]["question"] = res.list[0]["question"];
     this.data.questionList[0]["choices"] = [];
     for(var i=0;i<res.list[0]["choices"].length;i++){
-      this.data.questionList[0]["choices"].push({"answer":res.list[0]["choices"][i],"isTrue":res.list[0]["answer"].indexOf(res.list[0]["choices"][i])>-1,"class":"choice","selected":false});
+      this.data.questionList[0]["choices"].push({"answers":res.list[0]["choices"][i],"isTrue":res.list[0]["answers"].indexOf(res.list[0]["choices"][i])>-1,"class":"choice","selected":false});
     }
-    this.data.questionList[0]["answerNumber"] = res.list[0]["answer"].length;
+    this.data.questionList[0]["answerNumber"] = res.list[0]["answers"].length;
     console.log(this.data.questionList);
     this.setData({questionList:this.data.questionList});
     this.setData({end: false,submit: {"name":"提交","active":"submit"}});
