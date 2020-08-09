167a00bf08b468412f42cf7e8f086f080b4af7be
JueLuo <jueluo99@gmail.com>
2020-08-09 13:46:08 +0800

    增加了选择题库的功能
    题库会根据登录用户的权限进行开放

---
+++ cloudfunctions/getOpenId/index.js
@@ -0,0 +1,16 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  // API 调用都保持和云函数当前所在环境一致
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
+
+exports.main = (event, context) => {
+  // 这里获取到的 openId、 appId 和 unionId 是可信的，注意 unionId 仅在满足 unionId 获取条件时返回
+  let { OPENID, APPID, UNIONID } = cloud.getWXContext()
+
+  return {
+    OPENID
+  }
+}
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -4,22 +4,83 @@ const app = getApp()
 Page({
   data: {
     avatarUrl: '../../images/user-unlogin.png',
-    usingTools: [[{ "name": "练习", "active": "exercise", "type": 0 }, { "name": "模拟", "active": "exam", "type": 0 }], [{ "name": "客服", "type": 1 }]]
+    usingTools: [],
+    allTiku: [],
+    tikuData: [],
+    tikuIndex: 0,
+    openid: ""
   },
-  onShow: function(){
+
+  onShow: function() {
     this.onPullDownRefresh()
   },
-  exercise: function(){
+
+  exercise: function() {
     wx.navigateTo({
       url: '../exercise/exercise'
     })
   },
-  exam: function(){
+
+  exam: function() {
     wx.navigateTo({
       url: '../exam/exam'
     })
   },
+
+  // 实时响应切换题库
+  changeTiku: function(e) {
+    this.setData({ tikuIndex: e.detail.value })
+  },
+
+  // 找到题库与数据库名的对应关系
+  getAllTiku: function() {
+    const db = wx.cloud.database();
+    const $ = db.command;
+    db.collection('tikuName').get().then(res => {
+      // res.data 包含该记录的数据
+      console.log("拉取全部题库：", res.data)
+      this.setData({ allTiku: res.data })
+      this.getTiku();
+    });
+  },
+
+  // 获取当前用户有权限的题库列表
+  getTiku: function() {
+    const db = wx.cloud.database()
+    const $ = db.command
+    db.collection('users').where({ openid: this.data.openid }).get().then(res => {
+      console.log("用户所在组：", res.data)
+      var d = []
+      if (res.data.length == 0) {
+        return ''
+      }
+      console.log("所在组数量：", res.data[0].group.length)
+      for (var i = 0; i < res.data[0].group.length; i++) {
+        for (var j = 0; j < this.data.allTiku.length; j++) {
+          if (res.data[0].group[i].trim() == this.data.allTiku[j]["collection"].trim()) {
+            d.push(this.data.allTiku[j]["name"])
+          }
+        }
+      }
+      console.log("用户可访问题库：", d)
+      this.setData({ tikuData: d })
+    })
+  },
+
   onLoad: function() {
+    this.setData({ usingTools: [[]] })
+    // 调用云函数获取用户的 openid
+    wx.cloud.callFunction({
+      name: 'login',
+      complete: res => {
+        console.log("openid: ", res.result.openid)
+        this.setData({ openid: res.result.openid })
+        // 拉取全部题库信息
+        if (this.data.allTiku.length == 0) {
+          this.getAllTiku()
+        }
+      }
+    })
     // 获取用户信息
     wx.getSetting({
       success: res => {
@@ -32,18 +93,43 @@ Page({
                 userInfo: res.userInfo
               })
               this.setData({ isGetInfo: true })
+              this.setData({
+                usingTools: [
+                  [
+                    { "name": "练习", "active": "exercise", "type": 0 },
+                    { "name": "模拟", "active": "exam", "type": 0 }
+                  ],
+                  [
+                    { "name": "客服", "type": 1 }
+                  ]
+                ]
+              })
             }
           })
         }
       }
     })
-
-  }, 
-  getInfo:function(e){
-    this.setData({ userInfo: e.detail.userInfo, avatarUrl: e.detail.userInfo.avatarUrl, isGetInfo:true})
-    // 获取到并填充数据后自动刷新页面，避免用户手动刷新
-    this.onPullDownRefresh()
   },
-  onPullDownRefresh: function(e){
+
+  getInfo: function(e) {
+    this.setData({ userInfo: e.detail.userInfo, avatarUrl: e.detail.userInfo.avatarUrl, isGetInfo: true })
+    this.setData({
+        usingTools: [
+          [
+            { "name": "练习", "active": "exercise", "type": 0 },
+            { "name": "模拟", "active": "exam", "type": 0 }
+          ],
+          [
+            { "name": "客服", "type": 1 }
+          ]
+        ]
+      })
+      // 获取到并填充数据后自动刷新页面，避免用户手动刷新
+    this.onPullDownRefresh()
+
+  },
+
+  onPullDownRefresh: function(e) {
+
   }
 })
