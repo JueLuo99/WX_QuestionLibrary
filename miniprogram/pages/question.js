8a4363b8554c4719cff224edb1a3c73cd5b81e74
JueLuo <jueluo99@gmail.com>
2020-08-13 13:18:42 +0800

    新建了一个 component

---
+++ miniprogram/pages/question.js
@@ -0,0 +1,30 @@
+// pages/question.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    questionInfo:{
+      type: Object,
+      value: {},
+    },
+    badInfo: {
+      type: String,
+      value: "def",
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
