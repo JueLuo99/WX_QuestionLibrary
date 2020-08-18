cb969fe8adea65c12a4dd6c6980ae92896dc6229
GuokeNo1 <1962908113@qq.com>
2020-08-18 10:17:34 +0800

    完善了 component

--- miniprogram/pages/question.js
+++ miniprogram/pages/question.js
@@ -6,14 +6,13 @@ Component({
   properties: {
     questionInfo:{
       type: Object,
-      value: {},
+      value: {question:"",choices:[]},
     },
-    badInfo: {
-      type: String,
-      value: "def",
+    isEnd: {
+      type: Boolean,
+      value: false,
     }
   },
-
   /**
    * 组件的初始数据
    */
@@ -25,6 +24,33 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    selectItem:function(d){
+      if(this.data.isEnd){
+        return;
+      }
+      var qlist = this.data.questionInfo;
+      var qindex = d["currentTarget"]["dataset"]["qindex"];
+      var cindex = d["currentTarget"]["dataset"]["cindex"];
+      var status = qlist["choices"][cindex]["selected"];
+      if(qlist["answerNumber"]==1){
+        for(var i=0;i<qlist["choices"].length;i++){
+          var s = qlist["choices"][i]["selected"];
+          qlist["choices"][i]["selected"] = false;
+          qlist["choices"][i]["class"] = "choice";
+        }
+        qlist["choices"][cindex]["selected"] = true;
+        qlist["choices"][cindex]["class"] = "choice selected";
+      }else{
+        qlist["choices"][cindex]["selected"] = !status;
+        if(!status){
+          qlist["choices"][cindex]["class"] = "choice selected";
+        }else{
+          qlist["choices"][cindex]["class"] = "choice";
+        }
+      }
+      this.setData({questionInfo: qlist});
+      // 回调page
+      this.triggerEvent("Choice",this.data.questionInfo)
+    }
   }
 })
