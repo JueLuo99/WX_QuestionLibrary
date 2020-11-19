502b360e552cab01abfac0dfad1f8f918d1b9b1d
GuokeNo1 <1962908113@qq.com>
2020-11-19 10:43:48 +0800

    完善背题模式页面功能

--- miniprogram/pages/remember/remember.js
+++ miniprogram/pages/remember/remember.js
@@ -6,17 +6,45 @@ Page({
    */
   data: {
     endIndex: 0,
-    UIQuestions: []
+    UIQuestions: [],
+    tikuName:"",
+    noMore:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    if(options.tikuName == null){
+      wx.navigateBack()
+    }else{
+      this.setData({tikuName: options.tikuName})
+      this.updateData()
+    }
   },
   updateData:function(){
-
+    console.log("UpdateData is Run.")
+    const db = wx.cloud.database();
+    const $ = db.command;
+    db.collection(this.data.tikuName).skip(this.data.endIndex).get().then(res=>{
+      if(res.data.length<1){
+        this.setData({noMore:true})
+      }
+      for(var i=0;i<res.data.length;i++){
+        this.data.endIndex += 1
+        var newQuestion = {"question":"","choices":[]}
+        newQuestion.question = (this.data.endIndex+".["+ res.data[i].type +"] "+res.data[i].question).replace("[tf]","[判断题]").replace("[s]","[单选题]").replace("[m]","[多选题]")
+        for(var ci=0;ci<res.data[i].choices.length;ci++){
+          if(res.data[i].answers.indexOf(res.data[i].choices[ci])>-1){
+            newQuestion.choices.push({"answer":res.data[i].choices[ci],"isAnswer":true})
+          }else{
+            newQuestion.choices.push({"answer":res.data[i].choices[ci],"isAnswer":false})
+          }
+        }
+        this.data.UIQuestions.push(newQuestion)
+      }
+      this.setData({UIQuestions:this.data.UIQuestions})
+    })
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
