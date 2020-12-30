c648bfb50e9a22704cfdb0f5573c73a65b1ba1d2
GuokeNo1 <1962908113@qq.com>
2020-12-30 17:05:33 +0800

    新增云函数 教师登陆

---
+++ cloudfunctions/teacherLogin/index.js
@@ -0,0 +1,17 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  const db = cloud.database()
+  await db.collection('teacherLogin').doc(event.id).update({data:{login:wxContext.OPENID}})
+  return {
+    event,
+    openid: wxContext.OPENID,
+    appid: wxContext.APPID,
+    unionid: wxContext.UNIONID,
+  }
+}
